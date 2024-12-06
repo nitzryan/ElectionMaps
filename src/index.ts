f4f938b2016924f35971db5f92e49d86c604eb58
nitzryan <nitzryan@gmail.com>
2024-12-06 14:27:11 -0600

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const message = 'Hello, World';
+    const baseElement = document.createElement('h1')
+    baseElement.textContent = message;
+    document.body.appendChild(baseElement);
+});
